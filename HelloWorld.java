8c9d5c89c9c161aadbee79fdcbd866143fde58a2
priya232689 <priyankabhavsar564@gmail.com>
2025-08-19 13:20:16 +0530

    Sqaush and merge from Feature branch2 (#2)
    * Create HelloWorld.java
    
    * Update HelloWorld.java
    
    * Update HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.
+
+class HelloWorld{
+  public static void main(String args[]){
+    Sytem.out.println("Hi From feature branch 2");
+
+    System.out.println("Perform addition"+5+3+"=");
+    List <String> list_String=List.of("ls","cpy","cd',"mkdir");
+    System.out.println(list_String);
+  }
+}  
